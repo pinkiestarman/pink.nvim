5f83bbb63f49097fb7fe0f48855bfb40c6d5f7d7
pinkie <jansteenken128@gmail.com>
2025-04-15 20:54:47 +0200

    incremental selection

--- lua/pink/plugins/treesitter.lua
+++ lua/pink/plugins/treesitter.lua
@@ -33,11 +33,11 @@ return {
         additional_vim_regex_highlighting = { 'ruby' },
       },
       indent = { enable = true, disable = { 'ruby' } },
+      incremental_selection = { enable = true },
     },
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
     --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
