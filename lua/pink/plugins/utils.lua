0ac062f7d52c6100926cd6436762e26197034a2e
Jan Steenken <j.steenken@fp-dbs.com>
2025-10-06 12:19:06 +0200

    fix ripgrep

--- lua/pink/plugins/utils.lua
+++ lua/pink/plugins/utils.lua
@@ -1,23 +1,27 @@
 return {
-  'vimwiki/vimwiki',
-  'doums/rg.nvim',
-  cmd = { 'Rg', 'Rgf', 'Rgp', 'Rgfp' },
-  event = 'VeryLazy',
-  config = function()
-    require('rg').setup {
-      -- Optional function to be used to format the items in the
-      -- quickfix window (:h 'quickfixtextfunc')
-      qf_format = nil,
-      -- Glob list of excluded files and directories when the special
-      -- `e` flag is set (it uses the `--glob !*` flag of rg)
-      excluded = {
-        '.idea',
-        'node_modules',
-        '.git',
-        'target',
-        'package-lock.json',
-        'Cargo.lock',
-      },
-    }
-  end,
+  {
+    'vimwiki/vimwiki',
+    {
+      'doums/rg.nvim',
+      cmd = { 'Rg', 'Rgf', 'Rgp', 'Rgfp' },
+      event = 'VeryLazy',
+      config = function()
+        require('rg').setup {
+          -- Optional function to be used to format the items in the
+          -- quickfix window (:h 'quickfixtextfunc')
+          qf_format = nil,
+          -- Glob list of excluded files and directories when the special
+          -- `e` flag is set (it uses the `--glob !*` flag of rg)
+          excluded = {
+            '.idea',
+            'node_modules',
+            '.git',
+            'target',
+            'package-lock.json',
+            'Cargo.lock',
+          },
+        }
+      end,
+    },
+  },
 }
